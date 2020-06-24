0f67c5bdc927319f8a468df9eee0ea565424ec16
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-06-24 10:15:14 -0400

    turned htmlroutes into arrow functions

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
 
-router.get("/", function(req, res) {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // All other routes respond with the index.html file
-router.get("*", function(req, res) {
+router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
