423db8508acfd921109232d03d96451e9132cbea
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-06-25 16:18:48 -0400

    fixed server js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   // TODO: Finish updating this
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: process.env.DB_USER,
